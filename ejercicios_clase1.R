ffe8dc34d0d6088eef23ceb5bcdfde2acd0d7c89
Federico Castro Monzon <144367014+Federico-CM@users.noreply.github.com>
2023-09-12 09:58:51 -0600

    Create ejercicios_clase1.R
    Adding basic exercises.

---
+++ ejercicios_clase1.R
@@ -0,0 +1,125 @@
+# EJERCICIOS BASICOS CLASE 1 #
+
+# Trate de entender que hace cada linea
+# Antes de ejecutar cada linea trate de predecir lo que verá al ejecutarla
+# Añada notas después de cada linea, las notas comienzan con el símbolo #
+# Para ejecutar una linea en la cual está el cursor, apriete "control + enter"
+# Si encuentra un error al ejecutar una linea trate de entender cual es el error
+
+
+### OPERADORES BASICOS ###
+
+2+2
+
+"banana" + 2
+
+2-2
+
+2/2
+
+2^2
+
+2^3
+
+1:5
+
+1:5+1
+
+1:3^2
+
+(1:3)^2
+
+2 == 2
+
+8 > 2
+
+8 < 2
+
+2 >= 2
+
+2 <= 2
+
+2 == 1:3
+
+### CARACTERES ###
+
+"Esteban"
+
+"Esteban"+2
+
+"8"+2
+
+8+2
+
+"Esteban" == "Esteban"
+
+### VARIABLES ###
+amigo
+
+amigo <- "Esteban"
+
+amigo
+
+puntos
+
+puntos<-10
+
+puntos
+
+### VECTORES ###
+
+c("Esteban","Laura","Diego")
+
+alumno
+
+alumno<-c("Esteban","Laura","Diego")
+
+alumno
+
+calificacion
+
+calificacion<-8:10
+
+calificacion
+
+### TABLAS ###
+
+data.frame(alumno,calificacion)
+
+mi_lista
+
+mi_lista <- data.frame(alumnos,calificaciones)
+
+### SUBSELECCION ###
+
+alumno
+
+alumno[1]
+
+alumno[2]
+
+alumno[1:2]
+
+alumno[2:3]
+
+alumno[1:6]
+
+mi_lista
+
+mi_lista[1,]
+
+mi_lista[2,]
+
+mi_lista[,1]
+
+mi_lista[,2]
+
+mi_lista[1:2,]
+
+mi_lista[2:3,]
+
+mi_lista[,1:2]
+
+mi_lista[1,1]
+
+mi_lista[2,1]
